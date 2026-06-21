34cae1918aecc2900f44e1dba8636e0ebdfbc8b3
shaunakrk10 <shaunakrk10@gmail.com>
2026-06-20 21:23:55 -0400

    chore: set Vite base to '/OncoWispr/' (fix asset paths)

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/oncowispr/', // Matches your exact GitHub repository subfolder name
+  base: '/OncoWispr/', // Matches your exact GitHub repository subfolder name (case-sensitive)
 })
